4880a2b4d5136c3ae1e4149ae9abb2a49552ae42
zhengxisheng <gdpuzxs@163.com>
2017-07-18 17:41:42 +0800

    update
    update

--- src/main/java/springboot/web/HelloController.java
+++ src/main/java/springboot/web/HelloController.java
@@ -1,6 +1,7 @@
 package springboot.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +12,22 @@ public class HelloController {
 	
 	@Autowired
 	private User user;
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
 	
 	@RequestMapping("/hello")
 	public String hello(){
 		
 		return user.getName();
 	}
-	
 	@RequestMapping("/exception")
 	public String exception() throws Exception{
 	  throw new Exception("error");
 	}
+	@RequestMapping("/redisHandler")
+	public String redisHandler(){
+		stringRedisTemplate.opsForValue().set("k5", "Springboot redis");
+		return stringRedisTemplate.opsForValue().get("k5");
+	}
+	
 }
