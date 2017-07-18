a453f753086270ec1ca499b978a3329bc04943af
zhengxisheng <gdpuzxs@163.com>
2017-07-18 15:52:36 +0800

    add
    add

--- src/main/java/springboot/web/GlobalExceptionHandler.java
+++ src/main/java/springboot/web/GlobalExceptionHandler.java
@@ -4,12 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(value = Exception.class)
+	@ResponseBody
 	public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e){
 		ModelAndView modelAndView =new ModelAndView();
 		modelAndView.addObject("exception",e);
