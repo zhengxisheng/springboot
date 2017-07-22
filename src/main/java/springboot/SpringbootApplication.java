c3488faeaaa00595b88c1f75f0860351e8e61440
zhengxisheng <gdpuzxs@163.com>
2017-07-22 14:36:12 +0800

    war包部署tomcat
    war包部署tomcat

--- src/main/java/springboot/SpringbootApplication.java
+++ src/main/java/springboot/SpringbootApplication.java
@@ -2,15 +2,21 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootApplication.class);
+    }
 }
